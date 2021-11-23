1ad859ecea168b89ed41450d5989934d7ba072d2
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2021-11-23 16:08:47 -0700

    Build Christmas Eve Page

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,31 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter(),
+		target: '#svelte',
+		vite: {
+			resolve: {
+				alias: {
+					$css: path.resolve('./src/css'),
+					$components: path.resolve('./src/components'),
+					$data: path.resolve('./src/data'),
+					$img: path.resolve('./src/images')
+				}
+			}
+		}
 	}
 };
 
